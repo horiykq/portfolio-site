30baa17eb2bed2c3d9f85f8a967741097b07d663
horri1520 <kh1520.shsf@gmail.com>
2021-02-22 04:18:14 +0900

    add: add test index

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,59 @@
 <template>
-  <ps-separate-view :browsed-page="page">Welcome!</ps-separate-view>
+  <ps-separate-view :browsed-page="page">
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+    <ps-first-heading jp-heading="">Welcome to my portfolio!</ps-first-heading>
+  </ps-separate-view>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import PsFirstHeading from '~/components/common-components/ps-first-heading.vue'
 import PsSeparateView from '~/components/common-components/templates/ps-separate-view.vue'
 import { pages } from '~/constants'
 export default Vue.extend({
-  components: { PsSeparateView },
+  components: { PsSeparateView, PsFirstHeading },
 
   data() {
     return {
