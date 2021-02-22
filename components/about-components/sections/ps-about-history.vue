b3f48165ff9b47929f68bf6efa57625495607516
horri1520 <kh1520.shsf@gmail.com>
2021-02-22 23:32:41 +0900

    add: add history section

---
+++ components/about-components/sections/ps-about-history.vue
@@ -0,0 +1,14 @@
+<template>
+  <ps-section class="ps-about-history">
+    <ps-first-heading jp-heading="来歴">History</ps-first-heading>
+  </ps-section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PsFirstHeading from '~/components/common-components/ps-first-heading.vue'
+import PsSection from '~/components/common-components/ps-section.vue'
+export default Vue.extend({
+  components: { PsSection, PsFirstHeading },
+})
+</script>
