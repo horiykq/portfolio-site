879436ff45311af885d6ea08f876f291bf60e596
horri1520 <kh1520.shsf@gmail.com>
2021-02-22 16:29:57 +0900

    add: implement ps-image

--- components/about-components/sections/ps-about-profile.vue
+++ components/about-components/sections/ps-about-profile.vue
@@ -1,14 +1,16 @@
 <template>
   <ps-section>
-    <ps-first-heading jp-heading="プロフィール"> Profile </ps-first-heading>
+    <ps-first-heading jp-heading="プロフィール">Profile</ps-first-heading>
+    <ps-image src="/images/childhood.jpg" />
   </ps-section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import psFirstHeading from '~/components/common-components/ps-first-heading.vue'
+import PsImage from '~/components/common-components/ps-image.vue'
 import PsSection from '~/components/common-components/ps-section.vue'
 export default Vue.extend({
-  components: { psFirstHeading, PsSection },
+  components: { psFirstHeading, PsSection, PsImage },
 })
 </script>
---
+++ components/common-components/ps-image.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="ps-image" :src="imgSrc" decoding="async" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      default: undefined,
+    },
+  },
+
+  computed: {
+    imgSrc(): string {
+      if (this.src !== undefined) {
+        return this.src
+      }
+      return ''
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+$block: '.ps-image';
+#{$block} {
+  width: 100%;
+  height: 100%;
+}
+</style>
