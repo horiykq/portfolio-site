0ef6cd239e295349195239d93cafcc1fe6e30afd
horri1520 <kh1520.shsf@gmail.com>
2021-03-08 09:25:57 +0900

    chs

--- constants/sns/twitter.ts
+++ constants/sns/twitter.ts
@@ -1,8 +1,8 @@
 import sns from '~/types/sns'
 
 const twitter: sns = {
-  path: 'https://twitter.com/k__hori',
-  id: 'k__hori',
+  path: 'https://twitter.com/horri1520',
+  id: 'horri1520',
   name: 'Twitter',
 }
 
